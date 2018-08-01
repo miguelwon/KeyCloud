1e71f9cea0a645248b43e47776ff0a4efeff30aa
Miguel Won <miguelwon@tecnico.ulisboa.pt>
2018-08-01 11:26:17 +0100

    Initial commit

---
+++ keycloud/utils.py
@@ -0,0 +1,17 @@
+import nltk
+import string
+from itertools import chain
+from scipy.spatial import distance
+
+temp = string.punctuation+'\u2026'+'\xbb'+'\xab'+'\xba'+'“'+'’'+'”'+'‘'+'–'
+phorbidden = list(temp.replace('-','').replace('_',''))
+def asphorbidden(token):
+    return any(x in token for x in phorbidden)
+
+
+
+def candidate_chunks(text_pos,chunker):
+    chunks = chunker.parse(text_pos)
+    subtrees = chunks.subtrees(filter=lambda t: t.label() == 'NP')
+    temp = [[(x,tag) for x,tag in subtree.leaves()] for subtree in subtrees]
+    return temp
